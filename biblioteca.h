8430638b7a782467a95828cba8b723728acae457
Espinafres <leonorjoaquim98@gmail.com>
2025-04-17 13:50:42 +0100

    Still struggling

---
+++ biblioteca.h
@@ -0,0 +1,8 @@
+//
+// Created by leono on 4/10/2025.
+//
+
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+
+#endif //BIBLIOTECA_H
---
+++ main.c
@@ -0,0 +1,309 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <string.h>
+#include "biblioteca.h"
+#include <time.h>
+#include <ctype.h> 
+
+#define tam_perguntas 200
+#define tam_respostas 40
+
+typedef struct jogador{
+    char nome[20];
+    int score;
+}Jogador;
+
+typedef struct perguntas
+{
+    char pergunta[100][tam_perguntas];
+    char resposta[100][tam_perguntas];
+    int total;
+}Perguntas;
+
+void perguntasFaceis();
+//void randomPerguntas (char perguntas[], int numPerguntas);
+void instrucoes();
+void adicionarQuestoes();
+void perguntasMedias();
+
+int main() {
+    printf("***********************************************\n");
+    printf("*         Insira o seu nome ou nickname       *\n");
+    printf("***********************************************\n");
+    
+    Jogador jogador;
+    scanf("%s", jogador.nome);
+    system("cls");
+	int op; 
+    do {
+    printf("**********************************************************\n");
+    printf("                   Bem vindo %s\n", jogador.nome);
+    printf("**********************************************************\n");
+
+    printf("*********************************************************\n");
+    printf("*          Selecione o numero da opcao que quer:        *\n");
+    printf("*                                                       *\n");
+    printf("*     Qual o nivel de dificuldade que pretende jogar:   *\n");
+    printf("*       Inicial (1)     Medio (2)     Avancado (3)      *\n");
+    printf("*                                                       *\n");
+    printf("*        Instrucoes de utilizacao do quiz  (4)          *\n");
+    printf("*                                                       *\n");
+	printf("*            Historico e TOP 10 Jogadores  (5)          *\n");
+    printf("*                                                       *\n");
+	printf("*           Adicionar mais questoes ao quiz (6)         *\n");
+	printf("*                                                       *\n");
+    printf("*                 Sair do programa (7)                  *\n");
+    printf("*********************************************************\n");
+	
+	int contagem = 0;
+	scanf("%d", &op);
+	char sim;
+		switch (op) 
+		{
+			case 1:
+				system("cls");
+				printf("As perguntas faceis sao de verdadeiro e falso\nQuer prosseguir com as faceis?  (s/n)");
+				scanf(" %c", &sim);
+				if (sim == 's' || sim == 'S')
+					perguntasFaceis();
+				else
+					break;
+			case 2:
+				//system("cls");
+				printf("As perguntas medias sao de escolha multipla\nQuer prosseguir com as medias?  (s/n)");
+				scanf(" %c", &sim);
+				if (sim == 's' || sim == 'S')
+					perguntasMedias();
+				break;
+			case 3: 
+				//system("cls");
+				printf("As perguntas dificeis sao de resposta numerica\nQuer prosseguir com as dificeis?  (s/n)");
+				scanf(" %c", &sim);
+				//if (sim == 's' || sim == 'S')
+				//else
+				break;
+			case 4:
+				instrucoes();
+				break;
+			case 5:
+			case 6:
+				adicionarQuestoes();
+				break;
+			case 7:
+				//system("cls");
+				if(contagem == 0)
+					printf("Escolheu sair do programa. Obrigada na mesma! :)"); //questão de portugues
+				else if (contagem == 1)
+					printf("Escolheu sair do programa. Obrigada por jogar 1 vez! :)");
+				else
+					printf("Escolheu sair do programa. Obrigada por jogar %d vezes! :)", contagem);
+				return -1;   
+			default:
+				printf("\nOpcao Invalida\n");
+				Sleep(1000);
+				system("cls"); 
+				printf("Escolha outra opcao, por favor\n");
+				Sleep(2000);
+				system("cls"); 
+		}
+	} while( op != '7');
+	
+    FILE *jogadores = fopen("jogador1.txt", "a");
+    fprintf(jogadores, "\n%s", jogador.nome);
+    fclose(jogadores);
+}
+
+void perguntasFaceis() {
+	Perguntas p;
+    int c;
+    int numPerguntas;
+   	do {
+   	 	printf("Quantas perguntas quer responder?\nResposta: ");
+   		if(scanf("%d", &numPerguntas) == 1 && numPerguntas > 0)
+		   break;
+		while((c = getchar()) != '\n');
+   		printf("\nInsira por favor um numero positivo :)\n");
+	} while(1);
+	
+	int i = 0;
+	FILE *perguntas = fopen("perguntas.txt", "r");
+	char linha[tam_perguntas + tam_respostas + 5];
+	while (fgets(linha, sizeof(linha), perguntas) != NULL && i < numPerguntas) {
+		linha[strcspn(linha, "\n")] = 0;
+		
+		char pergunta[tam_perguntas];
+		char resposta[tam_respostas];
+		if (sscanf(linha, "%255[^,],%99[^\n]", pergunta, resposta) == 2) { //Ver outra forma 
+        	strncpy(p.pergunta[i], pergunta, tam_perguntas);
+        	printf("%s\n", pergunta);
+        	strncpy(p.resposta[i], resposta, tam_respostas);
+        	printf("%s\n", resposta);
+        	i++;
+    	} else {
+        	fprintf(stderr, "Linha inválida: %s\n", linha);
+    	}
+    }
+    p.total = i;
+    fclose(perguntas);
+}
+
+/*
+void randomPerguntas (char perguntas[], int total){
+	srand(time(NULL)); //função que não demos, dá reset ao rand() para que seja sempre random o baralhar
+	for (int i = total - 1; i > 0; i--) {
+		int j = rand() % (i + 1); // vai definir o numero random
+
+		char temp[tam_perguntas];
+		strcpy(temp, perguntas[i]); //aqui, ou usava pointers mas não demos, então com o string copy guardo aquela pergunta inicial numa variavel temporaria
+		strcpy(perguntas[i], perguntas[j]); //depois a pergunta selecionada pelo rand() é colocada na primeira posiçáo
+		strcpy(perguntas[j], temp); // e a primeira pergunta é colocada no lugar da selecionada pelo rand
+	}
+}
+*/
+
+void instrucoes() {
+	printf("***********************************************************\n");
+	printf("*      Escolha inicialmente a dificuldade pretendida      *\n");
+	printf("*   Vao aparecer 10 questoes sobre a cultura portuguesa   *\n");
+	printf("*               Todas de Verdadeiro ou Falso              *\n");
+	printf("*          E so colocar 'V' ou 'v' para verdadeiro        *\n");
+	printf("*          Ou entao 'F' ou 'f' se achar que e falsa       *\n");
+	printf("***********************************************************\n");
+	printf("\n\n          Pressione um botao para sair deste menu");
+	getch();
+	Sleep(1000);
+	system("cls");	
+}
+
+void adicionarQuestoes() {
+	char tipoPergunta;
+	char c;
+	//while(1){
+	//	printf("Qual é o nivel de dificuldade a que quer adicionar perguntas?")
+	//}
+	while(1) {
+		printf("Que tipo de perguntas quer acrescentar?\n Numerica (N)     V/F (V)    Escolha Multipla (E)");
+		while((c = getchar()) != '\n');
+		tipoPergunta = getchar();
+		tipoPergunta = toupper(tipoPergunta);
+		if(tipoPergunta == 'N' || tipoPergunta == 'V' || tipoPergunta == 'E') 
+			break;
+		printf("Caracter invalido! Experimente outro por favor");
+		Sleep(2000);
+		system("cls");
+	}
+	char perguntaAdicionar[tam_perguntas];
+	int respostaAdicionarNum = 0;
+	char respostaAdicionarChar;
+	char respostaAdicionarString[4][tam_respostas];
+	if (tipoPergunta == 'N'){
+		while((c = getchar()) != '\n');
+		printf("Escreva a pergunta que quer adicionar\n");
+		scanf(" %[^\n]", perguntaAdicionar);
+		//tamanhoPerguntaAdicionar = strlen(perguntaAdicionar);
+		//if (perguntaAdicionar[tamanhoPerguntaAdicionar - 1] != '?')
+		printf("Qual é a resposta certa? Sendo uma pergunta numerica, por favor coloque um numero");
+		while(1) {
+			if (scanf(" %d", &respostaAdicionarNum) == 1)
+				break;
+			printf("Nao e um numero! Por favor coloque um numero valido");
+		}
+		printf("Pergunta adicionada com sucesso! Obrigada :)");
+		FILE *perguntasNumericas = fopen("perguntas.txt", "a");
+		fprintf(perguntasNumericas, "%s,%d\n", perguntaAdicionar, respostaAdicionarNum);
+		fclose(perguntasNumericas);
+	}
+	else if (tipoPergunta == 'V') {
+		while((c = getchar()) != '\n');
+		printf("Escreva a pergunta quer adicionar: \n");
+		scanf(" %[^\n]", perguntaAdicionar);
+		printf("Qual é a resposta certa? Sendo uma pergunta de verdadeiro/falso, por favor coloque um V ou F\n");
+		while(1) {
+			scanf(" %c", &respostaAdicionarChar);
+			respostaAdicionarChar = toupper(respostaAdicionarChar);
+			if (respostaAdicionarChar == 'V' || respostaAdicionarChar == 'F')
+				break;
+			printf("Nao e um caracter valido! Por favor coloque V ou F :( \n");
+		}
+		printf("Pergunta adicionada com sucesso! Obrigada :)");
+		FILE *perguntasVerdadeiroFalso = fopen("perguntas.txt", "a");
+		fprintf(perguntasVerdadeiroFalso, "%s,%c\n", perguntaAdicionar, respostaAdicionarChar);
+		fclose(perguntasVerdadeiroFalso);
+	}
+	else {
+		printf("Escreva a pergunta quer adicionar: \n");
+		scanf(" %[^\n]", perguntaAdicionar);
+		printf("Qual é a resposta certa? Sendo uma pergunta de escolha multipla, por favor coloque 1 a opcao correta\ne depois pode colocar as falsas perfazer as 4");
+		int numRespostas = 0;
+		int i = 0;
+		while(numRespostas <= 3)
+		{
+			printf("\nOpcao %i", i + 1);
+			scanf(" %[^\n]", respostaAdicionarString[i]);
+			i++;
+			numRespostas++;
+		}
+		FILE *perguntasEscolhaMultipla = fopen("perguntas.txt", "a");
+		fprintf(perguntasEscolhaMultipla, "%s,%s,%s,%s,%s\n", perguntaAdicionar, respostaAdicionarString[0], respostaAdicionarString[1],respostaAdicionarString[2],respostaAdicionarString[3]);
+		fclose(perguntasEscolhaMultipla);
+	}
+}
+
+void perguntasMedias(){
+	FILE *perguntasMedias = fopen("perguntasmedias.txt", "r");
+	if (perguntasMedias == NULL) {
+		perror("Erro ao abrir o ficheiro de perguntas médias");
+		return;
+	} 
+	Perguntas p; 
+	printf("Estas perguntas são de Media dificuldade, valem 3 pontos cada. Quer mesmo prosseguir?"); //S ou S
+	char res;
+	scanf(" %c", &res);
+	res = tolower(res);
+	if (res != 's')
+		return;
+	printf("As perguntas vao aparecer de forma aleatoria ate ao numero que selecionou");
+	int numPerguntas;
+	char c;
+   	do {
+   	 	printf("Quantas perguntas quer responder?\nResposta: ");
+   		if(scanf("%d", &numPerguntas) == 1 && numPerguntas > 0)
+		   break;
+		while((c = getchar()) != '\n');
+   		printf("\nInsira por favor um numero positivo :)\n");
+	} while(1);
+	int i = 0;
+	char linha[tam_perguntas + tam_respostas + 5];
+	char tipo;
+char pergunta[tam_perguntas];
+char resposta1[tam_respostas];
+char resposta2[tam_respostas];
+char resposta3[tam_respostas];
+char resposta4[tam_respostas];
+
+while (fgets(linha, sizeof(linha), perguntasMedias) != NULL && i < numPerguntas) {
+    linha[strcspn(linha, "\n")] = 0;
+
+    tipo = linha[0];
+
+    if (tipo == '0' || tipo == '1') {
+        if (sscanf(linha + 1, "%255[^,],%99[^\n]", pergunta, resposta1) == 2) {
+            strncpy(p.pergunta[i], pergunta, tam_perguntas);
+            strncpy(p.resposta[i], resposta1, tam_respostas);
+            i++;
+        }
+    } else if (tipo == '2') {
+        if (sscanf(linha + 1, "%255[^,],%99[^,],%99[^,],%99[^,],%99[^\n]", pergunta, resposta1, resposta2, resposta3, resposta4) == 5) {
+            snprintf(p.pergunta[i], tam_perguntas, "%s", pergunta);
+            snprintf(p.resposta[i], tam_respostas, "%s,%s,%s,%s", resposta1, resposta2, resposta3, resposta4);
+            i++;
+        }
+    }
+}
+	for (int j = 0; j < numPerguntas; j++) {
+		printf("Pergunta %d: %s\n", j + 1, p.pergunta[j]);
+		printf("Resposta %d: %s\n", j + 1, p.resposta[j]);
+		}
+}
